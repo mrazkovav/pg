c8b11d61f98f81a445c96d35830ea1cfff1b921d
mrazkovav <mrazkovavendula@gmail.com>
2025-12-04 13:47:22 +0000

    ukol8

---
+++ def bin_to_dec(binarni_cislo):.py
@@ -0,0 +1,18 @@
+def bin_to_dec(binarni_cislo):
+    # funkce spocita hodnotu predavaneho binarniho cisla (binarni_cislo muze byt str i int!!!)
+    # 111 -> 7
+    # "101" -> 5
+binarni_cislo = str(binarni_cislo)
+
+for cislo in reversed(binarni_cislo):
+    if cislo not in ("0", "1"):
+        raise ValueError("vstup musi byt binarni cislo")
+
+
+def test_bin_to_dec():
+    assert bin_to_dec("0") == 0
+    assert bin_to_dec(1) == 1
+    assert bin_to_dec("100") == 4
+    assert bin_to_dec(101) == 5
+    assert bin_to_dec("010101") == 21
+    assert bin_to_dec(10000000) == 128
