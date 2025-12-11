c1867a9321a57401b48a5158f05330af52fc663f
mrazkovav <mrazkovavendula@gmail.com>
2025-12-11 13:08:45 +0100

    Add files via upload

---
+++ ninth.py
@@ -0,0 +1,14 @@
+def dec_to_bin(cislo):
+    # funkce prevede cislo na binarni reprezentaci (cislo muze byt str i int!!!)
+    # 7 -> "111"
+    # 5 -> "101"
+    return "0"
+
+
+def test_bin_to_dec():
+    assert dec_to_bin("0") == "0"
+    assert dec_to_bin(1) == "1"
+    assert dec_to_bin("100") == "1100100"
+    assert dec_to_bin(101) == "1100101"
+    assert dec_to_bin(127) == "1111111"
+    assert dec_to_bin("128") == "10000000"
